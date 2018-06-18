d6666a95885786d433299e57fae0282ba801de28
Zdeněk Balák <zdenek.balak@quanti.cz>
2018-06-18 16:17:40 +0200

    REF: delete unused import

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/FormElementValid.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/FormElementValid.kt
@@ -1,7 +1,5 @@
 package cz.qase.android.formbuilderlibrary.element
 
-import cz.qase.android.formbuilderlibrary.validator.FormValidator
-
 abstract class FormElementValid<T>(key: String) : FormElement<T>(key) {
 
     override fun validate(){
