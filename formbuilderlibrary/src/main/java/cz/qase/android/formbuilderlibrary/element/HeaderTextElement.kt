d44303c447a254f741be3ecda1fb106069930271
Zdeněk Balák <zdenek.balak@quanti.cz>
2018-06-18 14:12:46 +0200

    FIX: use root view to set correct layoutParams

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/HeaderTextElement.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/HeaderTextElement.kt
@@ -23,23 +23,24 @@ class HeaderTextElement(key: String,
     override fun createView(context: Context, formStyleBundle: FormStyleBundle): View {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(groupComponent, null) as ViewGroup
-        val headerView = prepareHeader(inflater, context, this.formStyleBundle ?: formStyleBundle)
-        val textView = prepareText(inflater, context, this.formStyleBundle ?: formStyleBundle)
+        val headerView = prepareHeader(inflater, context, this.formStyleBundle
+                ?: formStyleBundle, view)
+        val textView = prepareText(inflater, context, this.formStyleBundle ?: formStyleBundle, view)
         view.addView(headerView)
         view.addView(textView)
         return view
     }
 
-    private fun prepareText(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle): TextView {
-        val textView = inflater.inflate(textComponent, null) as TextView
+    private fun prepareText(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
+        val textView = inflater.inflate(textComponent, root, false) as TextView
         textView.setTextColor(context.resources.getColor(formStyleBundle.secondaryTextColor))
         textView.setBackgroundColor(context.resources.getColor(formStyleBundle.secondaryBackgroundColor))
         textView.text = value
         return textView
     }
 
-    private fun prepareHeader(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle): TextView {
-        val headerView = inflater.inflate(headerComponent, null) as TextView
+    private fun prepareHeader(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
+        val headerView = inflater.inflate(headerComponent, root, false) as TextView
         headerView.setTextColor(context.resources.getColor(formStyleBundle.primaryTextColor))
         headerView.setBackgroundColor(context.resources.getColor(formStyleBundle.primaryBackgroundColor))
         headerView.text = title
