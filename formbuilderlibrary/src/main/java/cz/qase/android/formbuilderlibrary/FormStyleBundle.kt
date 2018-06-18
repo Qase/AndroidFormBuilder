3587c616f3e0b7c92a05c657d099e5cc089a742c
Zdeněk Balák <zdenek.balak@quanti.cz>
2018-06-18 16:19:18 +0200

    FEAT: Divider

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/FormStyleBundle.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/FormStyleBundle.kt
@@ -3,4 +3,5 @@ package cz.qase.android.formbuilderlibrary
 class FormStyleBundle(val primaryTextColor: Int = R.color.colorPrimary,
                       val secondaryTextColor: Int = R.color.colorDefaultText,
                       val primaryBackgroundColor: Int = R.color.colorBackgroundPrimary,
-                      val secondaryBackgroundColor: Int = R.color.colorBackground)
+                      val secondaryBackgroundColor: Int = R.color.colorBackground,
+                      val dividerColor: Int = R.color.colorGreen)
---
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/Divider.kt
@@ -0,0 +1,22 @@
+package cz.qase.android.formbuilderlibrary.element
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import cz.qase.android.formbuilderlibrary.FormStyleBundle
+import cz.qase.android.formbuilderlibrary.R
+
+class Divider(key: String,
+              private val dividerComponent: Int = R.layout.form_divider) : FormElementValid<Unit>(key) {
+
+    override fun createView(context: Context, formStyleBundle: FormStyleBundle): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(dividerComponent, null)
+        view.setBackgroundColor(context.resources.getColor(formStyleBundle.dividerColor))
+        return view
+    }
+
+    override fun getVal() {
+
+    }
+}
