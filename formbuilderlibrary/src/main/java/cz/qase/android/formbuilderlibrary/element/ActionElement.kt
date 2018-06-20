10759cc6efddf44aa93bc832fff142303b6e9448
Zdeněk Balák <zdenek.balak@quanti.cz>
2018-06-20 15:16:17 +0200

    REF: refactor action element - merge it with label text action element

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/ActionElement.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/ActionElement.kt
@@ -1,29 +1,41 @@
 package cz.qase.android.formbuilderlibrary.element
 
 import android.content.Context
-import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
 import cz.qase.android.formbuilderlibrary.FormStyleBundle
 import cz.qase.android.formbuilderlibrary.R
 
-class ActionElement(private val label: String,
-                    private val actionCallback: ActionCallback,
-                    private val component: Int = R.layout.form_action) : FormElementValid<Unit>() {
 
-    override fun getVal() {
-
-    }
+class ActionElement(private val actionCallback: ActionCallback,
+                    label: String,
+                    text: String = "",
+                    groupComponent: Int = R.layout.form_group_item_inline,
+                    headerComponent: Int = R.layout.form_inline_label,
+                    textComponent: Int = R.layout.form_inline_text,
+                    formStyleBundle: FormStyleBundle? = null) :
+        LabelTextElement(label, text, groupComponent, headerComponent, textComponent, formStyleBundle) {
 
     override fun createView(context: Context, formStyleBundle: FormStyleBundle): View {
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val button = inflater.inflate(component, null) as Button
-        button.text = label
-        button.setBackgroundColor(context.resources.getColor(formStyleBundle.secondaryBackgroundColor))
-        button.setTextColor(context.resources.getColor(formStyleBundle.primaryTextColor))
-        button.setOnClickListener({
+        val view = super.createView(context, formStyleBundle)
+        view.setOnClickListener({
             actionCallback.callback()
         })
-        return button
+        view.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_UP -> {
+                    view.setBackgroundColor(context.resources.getColor(formStyleBundle.secondaryBackgroundColor))
+                    view.performClick()
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    view.setBackgroundColor(context.resources.getColor(formStyleBundle.secondaryBackgroundColor))
+                }
+                MotionEvent.ACTION_DOWN -> {
+                    view.setBackgroundColor(context.resources.getColor(formStyleBundle.primaryBackgroundColor))
+                }
+            }
+            true
+        }
+        return view
     }
 }
--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/LabelTextElement.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/LabelTextElement.kt
@@ -8,20 +8,19 @@ import android.widget.TextView
 import cz.qase.android.formbuilderlibrary.FormStyleBundle
 import cz.qase.android.formbuilderlibrary.R
 
-open class LabelTextElement(private val title: String,
-                            private val value: String,
+open class LabelTextElement(private val label: String,
+                            private val text: String,
                             private val groupComponent: Int = R.layout.form_group_item_inline,
                             private val headerComponent: Int = R.layout.form_inline_label,
                             private val textComponent: Int = R.layout.form_inline_text,
-                            private val formStyleBundle: FormStyleBundle? = null) : FormElementValid<String>() {
-    override fun getVal(): String? {
-        return value
+                            private val formStyleBundle: FormStyleBundle? = null) : FormElementValid<Unit>() {
+    override fun getVal() {
     }
 
     override fun createView(context: Context, formStyleBundle: FormStyleBundle): View {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(groupComponent, null) as ViewGroup
-        val headerView = prepareHeader(inflater, context, this.formStyleBundle
+        val headerView = prepareLabel(inflater, context, this.formStyleBundle
                 ?: formStyleBundle, view)
         val textView = prepareText(inflater, context, this.formStyleBundle ?: formStyleBundle, view)
         view.setBackgroundColor(context.resources.getColor(formStyleBundle.secondaryBackgroundColor))
@@ -33,14 +32,14 @@ open class LabelTextElement(private val title: String,
     private fun prepareText(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
         val textView = inflater.inflate(textComponent, root, false) as TextView
         textView.setTextColor(context.resources.getColor(formStyleBundle.secondaryTextColor))
-        textView.text = value
+        textView.text = text
         return textView
     }
 
-    private fun prepareHeader(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
+    private fun prepareLabel(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
         val headerView = inflater.inflate(headerComponent, root, false) as TextView
         headerView.setTextColor(context.resources.getColor(formStyleBundle.primaryTextColor))
-        headerView.text = title
+        headerView.text = label
         return headerView
     }
 }
