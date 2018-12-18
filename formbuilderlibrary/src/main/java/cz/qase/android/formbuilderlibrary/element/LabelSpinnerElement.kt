50edc91b5215031c381903714173396329b94d71
Zdeněk Balák <zdenek.balak@quanti.cz>
2018-12-18 19:02:38 +0100

    FIX: spinner - init value

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/LabelSpinnerElement.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/LabelSpinnerElement.kt
@@ -44,6 +44,7 @@ class LabelSpinnerElement<T>(private val label: String,
         spinner.setArrowDrawable(context.resources.getDrawable(arrowDrawable))
         spinner.setArrowTintColor(context.resources.getColor(formStyleBundle.secondaryTextColor))
         spinner.attachDataSource(availableValues)
+        spinner.selectedIndex = availableValues.indexOf(value)
         spinner.setTextColor(context.resources.getColor(formStyleBundle.secondaryTextColor))
         spinner.setBackgroundColor(context.resources.getColor(formStyleBundle.secondaryBackgroundColor))
         spinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
