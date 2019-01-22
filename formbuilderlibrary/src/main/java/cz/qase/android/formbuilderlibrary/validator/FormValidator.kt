a750bd7fd9dd501f3892642aff191f8cb83bffc5
Vladislav Trnka <vladislav.trnka@quanti.cz>
2019-01-22 14:32:02 +0100

    form validator interface description

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/validator/FormValidator.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/validator/FormValidator.kt
@@ -1,5 +1,11 @@
 package cz.qase.android.formbuilderlibrary.validator
 
+/**
+ * Basic interface for form input validating
+ */
 interface FormValidator<in T> {
+    /**
+     * Throw ValidationException when error otherwise do nothing
+     */
      fun validate(value: T?)
 }
