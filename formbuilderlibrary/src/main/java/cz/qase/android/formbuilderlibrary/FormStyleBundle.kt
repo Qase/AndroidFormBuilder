47f2051ba70030be56d2a24dc0802ebe00f68c8c
Vladislav Trnka <vladislav.trnka@quanti.cz>
2019-01-23 17:16:08 +0100

    added 4 more color palettes

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/FormStyleBundle.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/FormStyleBundle.kt
@@ -16,6 +16,30 @@ class FormStyleBundle(val primaryTextColor: Int = R.color.colorPrimary,
                 R.color.oneBackgroundSecondary,
                 R.color.oneDivider
         )
+
+        fun colorBundleTwo() = FormStyleBundle(
+                R.color.twoTextPrimary,
+                R.color.twoTextSecondary,
+                R.color.twoBackgroundPrimary,
+                R.color.twoBackgroundSecondary,
+                R.color.twoDivider
+        )
+
+        fun colorBundleThree() = FormStyleBundle(
+                R.color.threeTextPrimary,
+                R.color.threeTextSecondary,
+                R.color.threeBackgroundPrimary,
+                R.color.threeBackgroundSecondary,
+                R.color.threeDivider
+        )
+
+        fun colorBundleFour() = FormStyleBundle(
+                R.color.fourTextPrimary,
+                R.color.fourTextSecondary,
+                R.color.fourBackgroundPrimary,
+                R.color.fourBackgroundSecondary,
+                R.color.fourDivider
+        )
     }
 }
 
