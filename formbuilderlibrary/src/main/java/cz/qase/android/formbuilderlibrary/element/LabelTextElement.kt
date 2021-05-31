2a1e5665ef150bab0087977c4e2c74a791fbbf8c
balakz <zdenek.balak@quanti.cz>
2021-05-31 14:58:36 +0200

    FIX: LabelTextElement background

--- formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/LabelTextElement.kt
+++ formbuilderlibrary/src/main/java/cz/qase/android/formbuilderlibrary/element/LabelTextElement.kt
@@ -10,13 +10,16 @@ import cz.qase.android.formbuilderlibrary.R
 import cz.qase.android.formbuilderlibrary.common.setBackgroundColorResourceId
 import cz.qase.android.formbuilderlibrary.common.setTextColorResourceId
 import cz.qase.android.formbuilderlibrary.element.generic.FormElementNoValue
+import kotlinx.android.synthetic.main.form_header_item.view.*
 
-open class LabelTextElement(private val label: String,
-                            private val text: String,
-                            private val groupComponent: Int = R.layout.form_group_item_inline,
-                            private val headerComponent: Int = R.layout.form_inline_label,
-                            private val textComponent: Int = R.layout.form_inline_text,
-                            protected val formStyleBundle: FormStyleBundle? = null) : FormElementNoValue() {
+open class LabelTextElement(
+    private val label: String,
+    private val text: String,
+    private val groupComponent: Int = R.layout.form_group_item_inline,
+    private val headerComponent: Int = R.layout.form_inline_label,
+    private val textComponent: Int = R.layout.form_inline_text,
+    protected val formStyleBundle: FormStyleBundle? = null
+) : FormElementNoValue() {
 
     private var textView: TextView? = null
     private var labelView: TextView? = null
@@ -33,8 +36,10 @@ open class LabelTextElement(private val label: String,
     override fun createView(context: Context, formStyleBundle: FormStyleBundle): View {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(groupComponent, null) as ViewGroup
-        val headerView = prepareLabel(inflater, context, this.formStyleBundle
-                ?: formStyleBundle, view)
+        val headerView = prepareLabel(
+            inflater, context, this.formStyleBundle
+                ?: formStyleBundle, view
+        )
         val textView = prepareText(inflater, context, this.formStyleBundle ?: formStyleBundle, view)
         view.setBackgroundColorResourceId(context, formStyleBundle.secondaryBackgroundColor)
         view.addView(headerView)
@@ -49,7 +54,12 @@ open class LabelTextElement(private val label: String,
     override fun disableElement(context: Context, formStyleBundle: FormStyleBundle) {
     }
 
-    private fun prepareText(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
+    private fun prepareText(
+        inflater: LayoutInflater,
+        context: Context,
+        formStyleBundle: FormStyleBundle,
+        root: ViewGroup
+    ): TextView {
         val textView = inflater.inflate(textComponent, root, false) as TextView
         textView.setTextColorResourceId(context, formStyleBundle.secondaryTextColor)
         textView.text = text
@@ -65,9 +75,18 @@ open class LabelTextElement(private val label: String,
         textView?.text = text
     }
 
-    private fun prepareLabel(inflater: LayoutInflater, context: Context, formStyleBundle: FormStyleBundle, root: ViewGroup): TextView {
+    private fun prepareLabel(
+        inflater: LayoutInflater,
+        context: Context,
+        formStyleBundle: FormStyleBundle,
+        root: ViewGroup
+    ): TextView {
         val headerView = inflater.inflate(headerComponent, root, false) as TextView
         headerView.setTextColorResourceId(context, formStyleBundle.primaryTextColor)
+        headerView.setBackgroundColorResourceId(
+            context,
+            formStyleBundle.secondaryBackgroundColor
+        )
         headerView.text = label
         labelView = headerView
         return headerView
